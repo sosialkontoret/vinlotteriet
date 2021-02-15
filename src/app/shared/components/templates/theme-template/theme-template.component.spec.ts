70f6d4016d2cc677f119e8756756727807ddd01f
alexander <patricio.castillo@evry.com>
2021-02-15 18:53:39 +0100

    Setup theme

---
+++ src/app/shared/components/templates/theme-template/theme-template.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ThemeTemplateComponent } from './theme-template.component';
+
+describe('WineLotteryThemeTemplateComponent', () => {
+  let component: ThemeTemplateComponent;
+  let fixture: ComponentFixture<ThemeTemplateComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ThemeTemplateComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ThemeTemplateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/templates/theme-template/theme-template.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sk-theme-template',
+  templateUrl: './theme-template.component.html',
+  styleUrls: ['./theme-template.component.scss'],
+})
+export class ThemeTemplateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
