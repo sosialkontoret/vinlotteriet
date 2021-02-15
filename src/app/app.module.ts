50f5e7781d8c59b928777f56122e2b0230ebc2c5
Sindre Bøyum <sindre.boyum@gmail.com>
2021-02-15 20:46:26 +0100

    Init functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from 'app.component';
 import { environment } from '@env';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireFunctionsModule, USE_EMULATOR } from '@angular/fire/functions';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -13,11 +14,12 @@ import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
+    AngularFireFunctionsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     NoopAnimationsModule,
   ],
-  providers: [],
+  providers: [{ provide: USE_EMULATOR, useValue: ['localhost', 5001] }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
