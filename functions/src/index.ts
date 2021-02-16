758b1f9d5cd64b0a1c3d9b09a7c2c3b1a6f01beb
Sindre Bøyum <sindre.boyum@gmail.com>
2021-02-16 08:34:42 +0100

    Fix eslint troubles

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,10 @@
-import * as functions from 'firebase-functions';
+import { https, logger } from 'firebase-functions';
 
-import * as admin from 'firebase-admin';
+// import * as admin from 'firebase-admin';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
+export const helloWorld = https.onRequest((request, response) => {
+  logger.info('Hello logs!', { structuredData: true });
   response.send('Hello from Firebase!');
 });
