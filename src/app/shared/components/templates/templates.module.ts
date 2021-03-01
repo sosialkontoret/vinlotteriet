78cf9f84067b666136b704b0282d55dcd8a7dda7
alexander <patricio.castillo@evry.com>
2021-03-01 08:41:56 +0100

    Remove unused/invalid packages

--- src/app/shared/components/templates/templates.module.ts
+++ src/app/shared/components/templates/templates.module.ts
@@ -3,7 +3,6 @@ import { OrganismsModule } from '../organisms/organisms.module';
 import { LoginTemplateComponent } from './login-template/login-template.component';
 import { NewLotteryTemplateComponent } from './new-lottery-template/new-lottery-template.component';
 import { ThemeTemplateComponent } from './theme-template/theme-template.component';
-import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 import { HomeTemplateComponent } from './home-template/home-template.component';
 import { RegisterTemplateComponent } from './register-template/register-template.component';
 import { SharedComponentsModule } from '../shared-components.module';
@@ -11,8 +10,8 @@ import { MyLotteriesTemplateComponent } from './my-lotteries-template/my-lotteri
 import { UserTemplateComponent } from './user-template/user-template.component';
 import { EditLotteryTemplateComponent } from './edit-lottery-template/edit-lottery-template.component';
 import { LotteryTemplateComponent } from './lottery-template/lottery-template.component';
-import {DrawComponent} from '../deprecated/draw';
-import {WaitingToStartComponent} from '../deprecated/waiting-to-start';
+import { DrawComponent } from '../deprecated/draw';
+import { WaitingToStartComponent } from '../deprecated/waiting-to-start';
 
 const components = [
   EditLotteryTemplateComponent,
@@ -26,13 +25,11 @@ const components = [
   LotteryTemplateComponent,
 ];
 
-
 const deprecatedComponents = [DrawComponent, WaitingToStartComponent];
-
 
 @NgModule({
   declarations: [...components, ...deprecatedComponents],
-  imports: [OrganismsModule, SharedComponentsModule, OwlDateTimeModule, OwlNativeDateTimeModule],
+  imports: [OrganismsModule, SharedComponentsModule],
   exports: [...components],
 })
 export class TemplatesModule {}
