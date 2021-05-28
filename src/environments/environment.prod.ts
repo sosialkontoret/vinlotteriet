1dfbb1a55a7e1be8e55368735489302a9cf3599b
alexander <patricio.castillo@evry.com>
2021-05-28 14:54:09 +0200

    Commit environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+import { Environment } from './environment.type';
+
+export const environment: Environment = {
+  production: false,
+  version: '0.2',
+  firebase: {
+    apiKey: "AIzaSyCUXp_8EEN5Od13H1SmDCrHOmtjX2jVByY",
+    authDomain: "wine-lottery-cb86f.firebaseapp.com",
+    databaseURL: 'https://wine-lottery-prod.firebaseio.com',
+    projectId: "wine-lottery-cb86f",
+    storageBucket: "wine-lottery-cb86f.appspot.com",
+    messagingSenderId: "252449596356",
+    appId: "1:252449596356:web:b5af3be264bc0636f4fa91"
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+import { Environment } from './environment.type';
+
+export const environment: Environment = {
+  production: false,
+  version: '0.2',
+  firebase: {
+    apiKey: 'AIzaSyD_fLDbkhgdqFdH3Tye2Lbpqllr38aiYLg',
+    authDomain: 'wine-lottery-dev.firebaseapp.com',
+    databaseURL: 'https://wine-lottery-dev.firebaseio.com',
+    projectId: 'wine-lottery-dev',
+    storageBucket: 'wine-lottery-dev.appspot.com',
+    messagingSenderId: '314147254670',
+    appId: '1:314147254670:web:1a913132c1cbd11f27b4df',
+  },
+};
