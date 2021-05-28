d01e9649e396993b83908a982f32d366b0af2968
alexander <patricio.castillo@evry.com>
2021-05-28 15:44:17 +0200

    Add participant function + summarize lottery tickets

--- src/app/features/my-lotteries-feature/components/templates/edit-lottery-template/edit-lottery-template.component.ts
+++ src/app/features/my-lotteries-feature/components/templates/edit-lottery-template/edit-lottery-template.component.ts
@@ -1,25 +1,38 @@
-import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output, OnChanges } from '@angular/core';
 import { Lottery } from '@models/lottery.model';
 import { State } from '@models/enums/state.enum';
+import { Participant } from '@models/participant.model';
+import { ArrayUtils } from '@utils/array';
+import {LotteryUtils} from '@utils/lottery/lottery.utils';
 
 @Component({
   selector: 'sk-edit-lottery-template',
   templateUrl: './edit-lottery-template.component.html',
   styleUrls: ['./edit-lottery-template.component.scss'],
 })
-export class EditLotteryTemplateComponent implements OnInit {
+export class EditLotteryTemplateComponent implements OnInit, OnChanges {
   @Input() state: State;
   @Input() lottery: Lottery;
   @Output() updated = new EventEmitter<Lottery>();
 
-
-  get isLoading(): boolean {
-    return this.state === State.IsLoading;
-  }
-
-  constructor() {
-  }
+  numberOfTickets: number;
+  isLoading: boolean;
 
   ngOnInit() {
+    this.isLoading = this.state === State.IsLoading;
+    this.numberOfTickets = LotteryUtils.countNumberOfTickets(this.lottery);
+  }
+
+  ngOnChanges(): void {
+    this.isLoading = this.state === State.IsLoading;
+    this.numberOfTickets = LotteryUtils.countNumberOfTickets(this.lottery);
+  }
+
+  onAddParticipant(participant: Participant): void {
+    if (this.lottery.participants === undefined) {
+      this.lottery.participants = [];
+    }
+    this.lottery.participants.push(participant);
+    this.numberOfTickets = LotteryUtils.countNumberOfTickets(this.lottery);
   }
 }
