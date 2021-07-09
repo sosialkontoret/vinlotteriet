1ed6dbb7660599e68ed8aba548cebcb85be4d84e
Alexander Castillo <p.alexcast@gmail.com>
2021-07-09 19:19:33 +0200

    Remove unused code

--- src/app/features/home-feature/components/templates/lottery-template/lottery-template.component.ts
+++ src/app/features/home-feature/components/templates/lottery-template/lottery-template.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { Lottery } from '@models/lottery.model';
-import { DrawModel } from '@models/draw.model';
 
 @Component({
   selector: 'sk-lottery-template',
@@ -8,30 +7,5 @@ import { DrawModel } from '@models/draw.model';
   styleUrls: ['./lottery-template.component.scss'],
 })
 export class LotteryTemplateComponent {
-  @Input() lottery: Lottery;
-  currentDrawIndex = 0;
-  winners: string[] = [];
-
-  get numberOfDraws(): number {
-    return this.lottery?.draws?.length;
-  }
-
-  get currentDraw(): DrawModel {
-    if (!this.currentDrawIndex || !this.lottery?.draws) {
-      return undefined;
-    }
-    if (this.currentDrawIndex >= this.lottery?.draws?.length) {
-      return undefined;
-    }
-    return this.lottery.draws[this.currentDrawIndex];
-  }
-
-  get isOngoing(): boolean {
-    return !!this.currentDraw?.winner;
-  }
-
-  drawFinished() {
-    this.winners.push(this.currentDraw.winner);
-    this.currentDrawIndex += 1;
-  }
+  @Input() lottery: Lottery | undefined;
 }
